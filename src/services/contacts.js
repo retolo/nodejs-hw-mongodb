770c1983fa8dc07168ed46636408fa83a739bf73
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-09-20 16:23:30 +0300

    hw

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -11,3 +11,27 @@ export async function getContactById(contactId){
     const res = await ContactCollection.findById(contactId);
     return res;
 }
+
+export async function createContact(payload) {
+    const res = await ContactCollection.create(payload);
+    return res;
+
+}
+
+
+export async function deleContact(contactId) {
+    const res = ContactCollection.findByIdAndDelete(contactId)
+    return res
+
+}
+
+export async function upserContact(payload, contactId){
+    const res = await ContactCollection.findByIdAndUpdate(payload, contactId, {
+        new: true,
+        includeResultMetadata: true
+    });
+    return {
+        value: res.value,
+        updatedExisting: res.lastErrorObject.updatedExisting
+    };
+}
