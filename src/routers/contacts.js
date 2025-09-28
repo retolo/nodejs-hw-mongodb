7c3129082e886ec15cf1ac35a4aa9811984352c8
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-09-28 17:09:11 +0300

    hw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import isValidId from "../middlewares/isValidId.js";
 import {allContactsController, contactByIdController, createContactController, deleContactController, upsertContactController} from '../controllers/contacts.js'
-
+import { schemaCreate, schemaUpdate } from "../validation/contacts.js";
 const router = Router();
 
 
 router.get('/contacts', ctrlWrapper(allContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(contactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleContactController))
-router.patch('/contacts/:contactId', ctrlWrapper(upsertContactController))
+router.get('/contacts/:contactId',  isValidId, ctrlWrapper(contactByIdController));
+router.post('/contacts', validateBody(schemaCreate), ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', isValidId,  ctrlWrapper(deleContactController))
+router.patch('/contacts/:contactId', isValidId,  validateBody(schemaUpdate), ctrlWrapper(upsertContactController))
 export default router;
